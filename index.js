ae94a78cd654c37317dfc59c1d5c8148cadd20c7
Othman AMD <red.fantastic@hotmail.com>
2017-09-24 09:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n){
   var selector = document.querySelector('.ranked-list li');
   for (let i=0;i<selector.length;i++){
     var number = parseInt(selector[i].textContent,10)
-    selector[i].textContent = num + n;
+    selector[i].textContent = number + n;
   }
 }
