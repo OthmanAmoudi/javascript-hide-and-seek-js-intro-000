1013c4956a338b27107087bf3315883500dcf43f
Othman AMD <red.fantastic@hotmail.com>
2017-09-24 09:34:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function getFirstSelector(selector){
 function nestedTarget(){
   document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n){
+  var selector = document.querySelector('.ranked-list li');
+  for (let i=0;i<selector.length;i++){
+    selector+= parseInt(n);
+  }
+  
+}
+
