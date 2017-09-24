1f84b9fa4ebc0972686346c1801e75f201a08b3d
Othman AMD <red.fantastic@hotmail.com>
2017-09-24 09:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   var selector = document.querySelectorAll('.ranked-list li');
   for (let i=0;i<selector.length;i++){
-    let number = parseInt(selector[i].textContent,10)
+    var number = parseInt(selector[i].textContent,10)
     selector[i].textContent = number + n;
   }
 }
